1d4054f7ece822774122fd52a230b7947d19c287
Diego Becerril <diebecmag@outlook.com>
2023-11-09 21:18:12 -0600

    creación de modelos y cambio de BD

--- contactos/models.py
+++ contactos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contactos(models.Model):
+    nombre = models.CharField(max_length=80)
+    ap_pat = models.CharField(max_length=80)
+    ap_mat = models.CharField(max_length=80)
+    status = models.SmallIntegerField()
--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contactos(models.Model):
+    nombre = models.CharField(max_length=80)
+    ap_pat = models.CharField(max_length=80)
+    ap_mat = models.CharField(max_length=80)
+    status = models.SmallIntegerField()
+
+class Mensajes(models.Model):
+    texto = models.CharField(max_length=1000)
+    contacto_id = models.ForeignKey(Contactos,on_delete=models.PROTECT)
+    status = models.SmallIntegerField()
